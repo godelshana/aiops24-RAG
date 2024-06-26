41d7e9f6ac90e9893bb1ac82a8d7ac3622f0857d
zhangzc <1203947530@qq.com>
2024-06-26 17:08:15 +0800

    feat: 关键词混合检索

--- demo/main.py
+++ demo/main.py
@@ -7,13 +7,18 @@ from llama_index.llms.ollama import Ollama
 from llama_index.postprocessor.flag_embedding_reranker import (
     FlagEmbeddingReranker,
 )
+from llama_index.core.node_parser import MarkdownNodeParser
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.retrievers.bm25 import BM25Retriever
+from llama_index.core.schema import MetadataMode
 from qdrant_client import models
 from tqdm.asyncio import tqdm
+import jieba
 
 from pipeline.ingestion import build_pipeline, build_vector_store, read_data
 from pipeline.qa import read_jsonl, save_answers
-from pipeline.rag import QdrantRetriever, generation_with_knowledge_retrieval
-
+from pipeline.rag import QdrantRetriever, HybridRetriever, generation_with_knowledge_retrieval
+from custom.transformation import CustomFilePathExtractor, CustomTitleExtractor
 
 async def main():
     config = dotenv_values(".env")
@@ -29,7 +34,18 @@ async def main():
     )
     Settings.embed_model = embeding
 
-    reranker = FlagEmbeddingReranker(model="BAAI/bge-reranker-v2-m3", top_n=5, use_fp16=True)
+    reranker = FlagEmbeddingReranker(model="BAAI/bge-reranker-v2-m3", top_n=6, use_fp16=True)
+
+    # 关键词检索
+    data = read_data("data")
+    transformation = [
+        MarkdownNodeParser(include_metadata=True, include_prev_next_rel=True),
+        CustomTitleExtractor(metadata_mode=MetadataMode.EMBED),
+        CustomFilePathExtractor(last_path_length=4, metadata_mode=MetadataMode.EMBED),
+    ]
+    bm25Pipeline = IngestionPipeline(transformations=transformation)
+    nodes = await bm25Pipeline.arun(documents=data)
+    bm25Retriever = BM25Retriever.from_defaults(nodes=nodes,tokenizer=jieba.lcut, similarity_top_k=10)
 
     # 初始化 数据ingestion pipeline 和 vector store
     client, vector_store = await build_vector_store(config, reindex=False)
@@ -39,7 +55,6 @@ async def main():
     )
 
     if collection_info.points_count == 0:
-        data = read_data("data")
         pipeline = build_pipeline(llm, embeding, vector_store=vector_store)
         # 暂时停止实时索引
         await client.update_collection(
@@ -54,7 +69,9 @@ async def main():
         )
         print(len(data))
 
-    retriever = QdrantRetriever(vector_store, embeding, similarity_top_k=10)
+    qdranRetriever = QdrantRetriever(vector_store, embeding, similarity_top_k=10)
+
+    retriever = HybridRetriever(qdranRetriever, bm25Retriever)
 
     queries = read_jsonl("question.jsonl")
 
@@ -64,7 +81,7 @@ async def main():
     results = []
     for query in tqdm(queries, total=len(queries)):
         result = await generation_with_knowledge_retrieval(
-            query["query"], retriever, llm, reranker=reranker
+            query["query"], bm25Retriever, llm, # reranker=reranker
         )
         results.append(result)
 
--- demo/pipeline/rag.py
+++ demo/pipeline/rag.py
@@ -11,6 +11,8 @@ from llama_index.core import (
     StorageContext,
     VectorStoreIndex,
 )
+from llama_index.retrievers.bm25 import BM25Retriever
+from llama_index.core.node_parser import MarkdownNodeParser
 from llama_index.core.embeddings import BaseEmbedding
 from llama_index.core.retrievers import BaseRetriever
 from llama_index.core.schema import NodeWithScore
@@ -55,6 +57,37 @@ class QdrantRetriever(BaseRetriever):
             node_with_scores.append(NodeWithScore(node=node, score=similarity))
         return node_with_scores
 
+class HybridRetriever(BaseRetriever):
+    def __init__(self, vector_retriever, bm25_retriever):
+        self.vector_retriever = vector_retriever
+        self.bm25_retriever = bm25_retriever
+        super().__init__()
+
+    async def _aretrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
+        bm25_nodes = await self.bm25_retriever.aretrieve(query_bundle)
+        vector_nodes = await self.vector_retriever.aretrieve(query_bundle)
+
+        # combine the two lists of nodes
+        all_nodes = []
+        node_ids = set()
+        for n in bm25_nodes + vector_nodes:
+            if n.node.node_id not in node_ids:
+                all_nodes.append(n)
+                node_ids.add(n.node.node_id)
+        return all_nodes
+
+    def _retrieve(self, query_bundle: QueryBundle):
+        bm25_nodes = self.bm25_retriever.retrieve(query_bundle)
+        vector_nodes = self.vector_retriever.retrieve(query_bundle)
+
+        # combine the two lists of nodes
+        all_nodes = []
+        node_ids = set()
+        for n in bm25_nodes + vector_nodes:
+            if n.node.node_id not in node_ids:
+                all_nodes.append(n)
+                node_ids.add(n.node.node_id)
+        return all_nodes
 
 async def generation_with_knowledge_retrieval(
     query_str: str,
