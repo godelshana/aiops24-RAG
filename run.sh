4ccce2b8e33da422bc769e0d2dc8929ced1f58f6
zhangzc <1203947530@qq.com>
2024-06-26 11:35:25 +0800

    feat: 更新run脚本。增加权限，以及下载bge-m3和bge-reranker-v2-m3。

--- run.sh
+++ run.sh
@@ -5,10 +5,11 @@ mkdir bin
 
 # install ollama
 wget "https://modelscope.cn/api/v1/models/issaccv/OllamaDeploy/repo?Revision=master&FilePath=ollama-linux-amd64" -O bin/ollama
+
 chmod +x bin/ollama
 
 # run ollama in the background
-OLLAMA_FLASH_ATTENTION=1 bin/ollama serve &
+bin/ollama serve &
 
 # download model 
 wget "https://www.modelscope.cn/api/v1/models/qwen/Qwen2-7B-Instruct-GGUF/repo?Revision=master&FilePath=qwen2-7b-instruct-q8_0.gguf" -O qwen-2-7b-instruct.gguf
@@ -17,7 +18,8 @@ wget "https://www.modelscope.cn/api/v1/models/qwen/Qwen2-7B-Instruct-GGUF/repo?R
 bin/ollama create qwen -f modelfile
 
 # download the embedding model
-git clone https://www.modelscope.cn/AI-ModelScope/bge-small-zh-v1.5.git demo/BAAI/bge-small-zh-v1.5
+git clone https://www.modelscope.cn/Xorbits/bge-m3.git demo/BAAI/bge-m3
+git clone https://www.modelscope.cn/AI-ModelScope/bge-reranker-v2-m3.git demo/BAAI/bge-reranker-v2-m3
 
 # download dataset
 git clone https://www.modelscope.cn/datasets/issaccv/aiops2024-challenge-dataset.git demo/dataset
